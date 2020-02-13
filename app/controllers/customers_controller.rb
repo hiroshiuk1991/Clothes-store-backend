a80c7c73e45d0a19121796d1099c946806045f6a
hiroshiuk1991 <hiroshis91@icloud.com>
2020-02-13 12:30:43 +0000

    added login/signon

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -13,7 +13,7 @@ class CustomersController < ApplicationController
 
     def create 
         customer = Customer.create(customer_params)
-        render json: customer
+        render json: {customer_username: customer.username, customer_id: customer.id, token:issue_token({id: customer.id})}
     end 
 
     def destroy
@@ -30,7 +30,7 @@ class CustomersController < ApplicationController
         
         if customer and customer.authenticate(params[:customer][:password])
          
-            render json: {customer_username: customer.username, customer_id: customer.id, token:issue_token({id: customer.id}) }
+            render json: {customer_username: customer.username, customer_id: customer.id, token:issue_token({id: customer.id})}
         else 
           
             render json:{error: 'Username/Password invalid'}, status: 403    
@@ -40,7 +40,7 @@ class CustomersController < ApplicationController
     def validate 
         customer = get_current_customer
         if customer
-            render json: {customer: customer.username, token:issue_token({id: customer.id}) }
+            render json:{customer: customer.username, token:issue_token({id: customer.id}) }
         else
             render json:{error: 'Not Authorized'}, status: 401
         end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,6 @@
 class Customer < ApplicationRecord
     has_secure_password 
-    # has_unique_username
-
+   
     has_one :cart 
     has_many :items, through: :cart
 
